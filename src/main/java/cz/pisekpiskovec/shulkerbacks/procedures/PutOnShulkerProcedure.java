adbcdae080d4e05e67c2f5b740e17fe2bf9a89cd
Písek Pískovec <pisekpiskovec@gmail.com>
2023-02-27 22:05:26 +0100

    remade the mod

---
+++ src/main/java/cz/pisekpiskovec/shulkerbacks/procedures/PutOnShulkerProcedure.java
@@ -0,0 +1,56 @@
+package cz.pisekpiskovec.shulkerbacks.procedures;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Hand;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.item.ItemStack;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+import cz.pisekpiskovec.shulkerbacks.PiseksShulkerbacksMod;
+
+public class PutOnShulkerProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				PiseksShulkerbacksMod.LOGGER.warn("Failed to load dependency entity for procedure PutOnShulker!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		ItemStack mainHand = ItemStack.EMPTY;
+		ItemStack chestSlot = ItemStack.EMPTY;
+		double mainHandAmount = 0;
+		double chestSlotAmount = 0;
+		if (ItemTags.getCollection().getTagByID(new ResourceLocation("piseks_shulkerbacks:wearable_blocks"))
+				.contains(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem())) {
+			mainHand = (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).copy());
+			chestSlot = (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.CHEST) : ItemStack.EMPTY)
+					.copy());
+			mainHandAmount = ((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)).getCount());
+			chestSlotAmount = ((((entity instanceof LivingEntity)
+					? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.CHEST)
+					: ItemStack.EMPTY)).getCount());
+			if (entity instanceof LivingEntity) {
+				if (entity instanceof PlayerEntity)
+					((PlayerEntity) entity).inventory.armorInventory.set((int) 2, (mainHand));
+				else
+					((LivingEntity) entity).setItemStackToSlot(EquipmentSlotType.CHEST, (mainHand));
+				if (entity instanceof ServerPlayerEntity)
+					((ServerPlayerEntity) entity).inventory.markDirty();
+			}
+			if (entity instanceof LivingEntity) {
+				ItemStack _setstack = (chestSlot);
+				_setstack.setCount((int) chestSlotAmount);
+				((LivingEntity) entity).setHeldItem(Hand.MAIN_HAND, _setstack);
+				if (entity instanceof ServerPlayerEntity)
+					((ServerPlayerEntity) entity).inventory.markDirty();
+			}
+		}
+	}
+}
